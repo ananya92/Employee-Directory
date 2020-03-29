2b1cc92f939d8164fb28a59a9f9ccef55b9b9229
ananya92 <1992ananya@gmail.com>
2020-03-30 06:16:50 +1030

    Changed DropdownSort into a functional component and used the store context to invoke dispatch method to update the store whenever a new sort option is selected

--- employee_directory/src/components/DropdownSort.js
+++ employee_directory/src/components/DropdownSort.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
+import { useEmployeeContext } from "../utils/GlobalState";
 
 const options = [
   { value: 'Employee Name', label: 'Employee Name' },
@@ -7,28 +8,38 @@ const options = [
   { value: 'Designation', label: 'Designation' },
 ];
 
-class DropdownSort extends React.Component {
-  state = {
-    selectedOption: null,
-  };
-  handleChange = selectedOption => {
-    this.setState(
-      { selectedOption },
-      () => console.log(`Option selected:`, this.state.selectedOption)
-    );
-  };
-  render() {
-    const { selectedOption } = this.state;
+function DropdownSort() {
+  const [state, dispatch] = useEmployeeContext();
 
-    return (
-      <Select
-        placeholder={"Sort by"}
-        value={selectedOption}
-        onChange={this.handleChange}
-        options={options}
-      />
+  // using useState hook to save the selected sort option
+  const [sortState, setSortState] = useState({
+      selectedOption: "",
+  });
+
+  // using useEffect hook to observe the state of sortState and call the dispatch function with sort action to update the store
+  useEffect(() => {
+    dispatch({type: "sort", data: sortState.selectedOption});
+  }, [sortState]);
+
+  // using useEffect hook to observe the filters in the store and clear the sort option field whenever the state of the filters changes
+  useEffect(() => {
+    setSortState({ selectedOption: "" })
+  }, [state.filters]);
+
+  // handleChange function to update the sort state when value from the dropdown is selected
+  const handleChange = selectedOption => {
+    setSortState(
+      { selectedOption: selectedOption }
     );
-  }
+  };
+  return (
+    <Select
+      placeholder={"Sort by"}
+      value={sortState.selectedOption}
+      onChange={handleChange}
+      options={options}
+    />
+  );
 }
 
 export default DropdownSort;
