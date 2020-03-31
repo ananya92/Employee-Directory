2d933889b351ff82437ff8c61b94bbe1728956ff
ananya92 <1992ananya@gmail.com>
2020-03-31 18:04:21 +1030

    Fix attempt blank screen

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./employee-directory/build/index.html"));
-});
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/empDirectory");
